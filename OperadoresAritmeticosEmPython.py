d2ff6146c42ddaf6ae50b4498a20711183bd3cf9
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2021-11-25 13:19:25 -0300

    Update

---
+++ OperadoresAritmeticosEmPython.py
@@ -0,0 +1,16 @@
+###     Operadores Aritmeticos
+#       + --> Adição
+#       - --> Subtração
+#       * --> Multiplicação
+#       / --> Divisão
+#       ** --> Potência
+#       // --> Divisão Inteira
+#       %  --> Resto da Divisão (MOD)
+#       Todos os Operadores precisam de dois operadores (Operadores Binários)
+#       
+#       Ordem de Precedência
+#   1-) ()
+#   2-) **
+#   3-) *   /    //  %
+#   4-) +   -
+###
--- aula6a.py
+++ aula6a.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite um valor: '))#observacao colocar o tipo de dados, porque 
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
 #print('A soma entre', n1, "e", n2 , 'vale', soma)
-print('A soma entre {} e {} vale {}'.format(n1, n2, soma)) #sintaxe nova com metodo .format()`
+print(f'A soma entre {n1} e {n2} vale {soma}') #sintaxe nova com metodo .format()` ou colocar f antes das '' e colocar os parametros dentro das chaves!
