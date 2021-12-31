1c5b39153200caada7a6da3e532c6e95dd7554a5
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2021-12-31 18:10:57 -0300

    Exercio para aplcicar desconto em produto

--- Desafios/DescontoProduto.py
+++ Desafios/DescontoProduto.py
@@ -1,4 +1,16 @@
 #Crie um programa que calcule determinado(Qualquer valor) desconto,
 # e aplique esse desconto no final da comprar daquele usuario
 
-nome = str(input('Seja Bem-Vindo!\nInfome seu nome completo, por favor.'))
+#Entrada de Dados
+nomeC = str(input('Seja Bem-Vindo!\nInfome seu nome completo, por favor.')).upper()
+nomeProduto = str(input('Informe o nome do produto: ')).upper()
+valorP = float(input('Informe o valor do produto: R$'))
+valorDesconto = float(input('Informe o valor do desconto: Por exemplo 10% ou 15%'))
+
+#Processamento de dados
+
+desconto = valorP - valorP * (valorDesconto / 100)
+
+# Saída de Dados
+
+print(f'Nome do cliente: {nomeC}\nNome do Produto: {nomeProduto}\nValor do Produto: {valorP:.2f}\nValor do desconto aplicado: {valorDesconto:.2f}\nValor Final: {desconto:.2f}')
