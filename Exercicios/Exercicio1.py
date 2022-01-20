e60c06e1bad888e4f080e65b82d44c65539ef5e4
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2022-01-20 12:35:03 -0300

    Fazendo alguns exercicios

---
+++ Exercicios/Exercicio1.py
@@ -0,0 +1,15 @@
+##1º Exercício
+# Faça um programa que informe a versão do Python que você está utilizando.
+    
+import sys
+
+print("Versão do Python:")
+
+print (sys.version)
+
+print("Informação da versão instalada:")
+
+print (sys.version_info)
+
+##Observações: - O módulo sys fornece funções e variáveis usadas para manipular diferentes 
+# partes do ambiente de tempo de execução do Python;
---
+++ Exercicios/Exercicio2.py
@@ -0,0 +1,8 @@
+##2º Exercício
+# Faça um programa em linguagem Python que converta metros para centímetros.
+
+print('Seja Bem-Vindo! Ao programa de conversão de medidas...')
+
+metros = int(input('Informe o valor em metros.'))
+
+print (f'O valor em centimetros é: {metros * 100}cm.')
