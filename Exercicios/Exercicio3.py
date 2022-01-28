9dea9ae1fdf30f14c1d8ed83d92e155172e8728e
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2022-01-28 04:32:08 -0300

    Update Exercicio3.py

--- Exercicios/Exercicio3.py
+++ Exercicios/Exercicio3.py
@@ -8,7 +8,9 @@ n = int(input('Informe o número para ver a tabuada do respectivo número:'))
 print(f'\n Tabuada de {n}:')
 
 for i in range(1, 11):
-    print(n,'X', i, '=', (n * i))
+   # print(n,'X', i, '=', (n * i))
+
+    print(f'{n} X {i} = {n*i}')
     
     ## O for é utilizado para percorrer um valor dentro de um alcançe determinado! 
     # (O famoso contador) ele vai contar o valor da variavel ou constante!
