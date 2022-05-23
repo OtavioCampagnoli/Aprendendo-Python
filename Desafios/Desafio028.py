f9745b85a8c1159b1f7e9c4aac898b99d94d90cc
OtavioCampagnoli <otaviocampagnoli@hotmail>
2022-05-23 10:53:23 -0300

    Desafio condicional -Exercicio 028

--- Desafios/Desafio028.py
+++ Desafios/Desafio028.py
@@ -1,9 +1,14 @@
-#Escreva um programa que faça o computador "pensar" em número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+#Escreva um programa que faça o computador "pensar" em número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.O programa deverá escrever na tela se o usuário venceu ou perdeu.
 
-#O programa deverá escrever na tela se o usuário venceu ou perdeu.
+from random import randint
 
-from random import random
-
-n = int
-random(n)
-print(n)
+computador = randint(0, 5) # faz o computador sortear entre 0 - 5
+print('')
+print('-=-' * 20)
+print('Irei pensar em entre 0 e 5.')
+print('-=-' * 20)
+jogador = int(input('Tente advinhar o número que pensei!'))
+if(jogador == computador):
+    print(f'Parabéns você acertou eu pensei em: {computador}.')
+else:
+    print(f'Poxa que pena foi quase... Eu tinha pensado em {computador}.')
